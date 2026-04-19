7d472244aa3c23811383d31e0c56045f0b26655c
Daniil <Fedotov.D.V2006@mail.ru>
2026-04-19 21:28:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,82 @@
 // Copyright 2025 NNTU-CS
 #include <string>
-#include <map>
+#include <cctype>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
-}
-
-int eval(const std::string& pref) {
-  // добавьте код
+int priority(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
   return 0;
+}
+bool is_op(char c) {
+  return c == '+' || c == '-' || c == '*' || c == '/';
+}
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char, 100> st;
+  std::string res;
+  for (size_t i = 0; i < inf.size(); i++) {
+    char c = inf[i];
+    if (c == ' ') continue;
+    if (isdigit(c)) {
+      while (i < inf.size() && isdigit(inf[i])) {
+        res += inf[i];
+        i++;
+      }
+      res += ' ';
+      i--; 
+    }
+    else if (c == '(') {
+      st.Push(c);
+    }
+    else if (c == ')') {
+      while (!st.IsEmpty() && st.Top() != '(') {
+        res += st.Top();
+        res += ' ';
+        st.Pop();
+      }
+      st.Pop();
+    }
+    else if (is_op(c)) {
+      while (!st.IsEmpty() && priority(st.Top()) >= priority(c)) {
+        res += st.Top();
+        res += ' ';
+        st.Pop();
+      }
+      st.Push(c);
+    }
+  }
+  while (!st.IsEmpty()) {
+    res += st.Top();
+    res += ' ';
+    st.Pop();
+  }
+  return res;
+}
+int eval(const std::string& post) {
+  TStack<int, 100> st;
+  for (size_t i = 0; i < post.size(); i++) {
+    if (post[i] == ' ') continue;
+    if (isdigit(post[i])) {
+      int num = 0;
+      while (i < post.size() && isdigit(post[i])) {
+        num = num * 10 + (post[i] - '0');
+        i++;
+      }
+      st.Push(num);
+      i--;
+    }
+    else {
+      int b = st.Top(); st.Pop();
+      int a = st.Top(); st.Pop();
+      int res = 0;
+      switch (post[i]) {
+        case '+': res = a + b; break;
+        case '-': res = a - b; break;
+        case '*': res = a * b; break;
+        case '/': res = a / b; break;
+      }
+      st.Push(res);
+    }
+  }
+  return st.Top();
 }
